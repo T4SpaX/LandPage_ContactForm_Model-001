853b7ea2a3eb8d889565cc6dfbf9106b24682f41
T4SpaX <playerx173@gmail.com>
2022-09-11 17:36:32 +0000

    new dev things

---
+++ components/Buttons/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as C from '../../styles/components_Styles/Buttons_style';
+
+const Buttons = () => {
+  return (
+    <C.Anchor href='/contato'>
+      <C.Button>
+        Quero Fazer Meu Orçamento !
+      </C.Button>
+    </C.Anchor>
+   
+  )
+}
+export default Buttons;
